566ef0fbf329845b450a8c1182ceaae4e18a4e3e
junxiangzhan <96927456+junxiangzhan@users.noreply.github.com>
2025-02-05 23:06:33 +0800

    Remove Default Content

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
+    <StrictMode>Hello World</StrictMode>
+);
